0b640899dfd07d0a97be56b782494472b8c21ded
Florian Finkernagel <finkernagel@imt.uni-marburg.de>
2024-07-11 16:35:57 +0200

    CI: Split giant darwin job into multiple subjobs.

--- flake.nix
+++ flake.nix
@@ -14,78 +14,87 @@
     };
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    , nix-github-actions
-    , treefmt-nix
-    , systems
-    ,
-    }:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-      treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./dev/treefmt.nix);
-    in
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nix-github-actions,
+    treefmt-nix,
+    systems,
+  }: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    treefmtEval = eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./dev/treefmt.nix);
+  in
     {
-      overlays.default = nixpkgs.lib.composeManyExtensions [ (import ./overlay.nix) ];
-      lib.mkPoetry2Nix = { pkgs }: import ./default.nix { inherit pkgs; };
+      overlays.default = nixpkgs.lib.composeManyExtensions [(import ./overlay.nix)];
+      lib.mkPoetry2Nix = {pkgs}: import ./default.nix {inherit pkgs;};
 
-      githubActions =
-        let
-          mkPkgs = system:
-            import nixpkgs {
-              config = {
-                allowAliases = false;
-                allowInsecurePredicate = _: true;
-              };
-              overlays = [ self.overlays.default ];
-              inherit system;
+      githubActions = let
+        mkPkgs = system:
+          import nixpkgs {
+            config = {
+              allowAliases = false;
+              allowInsecurePredicate = _: true;
             };
-        in
+            overlays = [self.overlays.default];
+            inherit system;
+          };
+      in
         nix-github-actions.lib.mkGithubMatrix {
           platforms = {
             "x86_64-linux" = "ubuntu-22.04";
             "x86_64-darwin" = "macos-13";
             "aarch64-darwin" = "macos-14";
           };
-          checks = {
-            x86_64-linux =
-              let
-                pkgs = mkPkgs "x86_64-linux";
-              in
-              import ./tests { inherit pkgs; }
+          checks = let
+            splitList = x: list: let
+              length = builtins.length list;
+              divisor = nixpkgs.lib.trivial.min x length;
+              chunkSize = length / divisor;
+              indices = builtins.genList (i: i * chunkSize) divisor;
+              lastChunk = nixpkgs.lib.lists.last indices;
+            in
+              builtins.map (i:
+                nixpkgs.lib.lists.sublist i
+                (
+                  if (i == lastChunk)
+                  then length
+                  else chunkSize
+                )
+                list)
+              indices;
+            # Aggregate all tests into a small number of  derivations so that only a small number of GHA runners is scheduled for all darwin jobs
+            # (a single one runs >4h hours)
+
+            mkDarwinTests = pkgs: let
+              inherit (pkgs) lib;
+              tests = import ./tests {inherit pkgs;};
+              all_tests = lib.attrValues (lib.filterAttrs (_: v: lib.isDerivation v) tests);
+              split_count = 8;
+              split_tests = splitList split_count all_tests; # that's the number of jobs we split this into.
+              split_test_derivations = map (sub_tests:
+                pkgs.runCommand "darwin-aggregate"
+                {
+                  env.TEST_INPUTS = lib.concatStringsSep " " sub_tests;
+                } "touch $out")
+              split_tests;
+            in
+              builtins.listToAttrs (map (ii: {
+                name = "darwin-aggregate" + (builtins.toString ii);
+                value = builtins.elemAt split_test_derivations ii;
+              }) (lib.lists.range 0 (split_count - 1)));
+          in {
+            x86_64-linux = let
+              pkgs = mkPkgs "x86_64-linux";
+            in
+              import ./tests {inherit pkgs;}
               // {
                 formatting = treefmtEval.x86_64-linux.config.build.check self;
               };
 
-            x86_64-darwin =
-              let
-                pkgs = mkPkgs "x86_64-darwin";
-                inherit (pkgs) lib;
-                tests = import ./tests { inherit pkgs; };
-              in
-              {
-                # Aggregate all tests into one derivation so that only one GHA runner is scheduled for all darwin jobs
-                aggregate = pkgs.runCommand "darwin-aggregate"
-                  {
-                    env.TEST_INPUTS = lib.concatStringsSep " " (lib.attrValues (lib.filterAttrs (_: v: lib.isDerivation v) tests));
-                  } "touch $out";
-              };
-            aarch64-darwin =
-              let
-                pkgs = mkPkgs "aarch64-darwin";
-                inherit (pkgs) lib;
-                tests = import ./tests { inherit pkgs; };
-              in
-              {
-                # Aggregate all tests into one derivation so that only one GHA runner is scheduled for all darwin jobs
-                aggregate =
-                  pkgs.runCommand "darwin-aggregate"
-                    {
-                      env.TEST_INPUTS = lib.concatStringsSep " " (lib.attrValues (lib.filterAttrs (_: v: lib.isDerivation v) tests));
-                    } "touch $out";
-              };
+            x86_64-darwin = mkDarwinTests (mkPkgs "x86_64-darwin");
+
+            aarch64-darwin = mkDarwinTests (mkPkgs "aarch64-darwin");
           };
         };
 
@@ -97,17 +106,15 @@
         default = self.templates.app;
       };
     }
-    // (flake-utils.lib.eachDefaultSystem (system:
-    let
+    // (flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
         config.allowAliases = false;
       };
 
-      poetry2nix = import ./default.nix { inherit pkgs; };
-      p2nix-tools = pkgs.callPackage ./tools { inherit poetry2nix; };
-    in
-    rec {
+      poetry2nix = import ./default.nix {inherit pkgs;};
+      p2nix-tools = pkgs.callPackage ./tools {inherit poetry2nix;};
+    in rec {
       formatter = treefmtEval.${system}.config.build.wrapper;
 
       packages = {
@@ -137,7 +144,7 @@
           type = "app";
           program = pkgs.poetry;
         };
-        poetry2nix = flake-utils.lib.mkApp { drv = packages.poetry2nix; };
+        poetry2nix = flake-utils.lib.mkApp {drv = packages.poetry2nix;};
         default = apps.poetry2nix;
       };
     }));
